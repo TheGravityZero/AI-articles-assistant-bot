95e6e7ce762853b500dad6e3471c020c1be5f15a
mosikk <moiseenkov_ilya@mail.ru>
2023-12-10 15:04:16 +0300

    add bot

---
+++ settings.py
@@ -0,0 +1,70 @@
+import logging
+import os
+
+# Log configuration
+logger = logging.getLogger("ai_articles_assistant")
+logger.setLevel(os.getenv("LOG_LEVEL", "INFO"))
+
+# Telegram bot configuration
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    logging.error(
+        "BOT_TOKEN env var is not found"
+    )
+else:
+    logging.info("BOT_TOKEN found, starting the bot")
+
+# Hugging face token
+HF_TOKEN = os.getenv("HF_TOKEN")
+if not HF_TOKEN:
+    logging.error(
+        "HF_TOKEN env var is not found"
+    )
+else:
+    logging.info("HF_TOKEN found")
+
+# Pinecon token
+PINECONE_TOKEN = os.getenv("PINECONE_TOKEN")
+if not PINECONE_TOKEN:
+    logging.error(
+        "PINECONE_TOKEN env var is not found"
+    )
+else:
+    logging.info("PINECONE_TOKEN found")
+
+
+# Model configuration
+MODEL_ID = "meta-llama/Llama-2-13b-chat-hf"
+logging.info(f"MODEL_ID is {MODEL_ID}")
+
+INDEX_ID = "llama-2-rag"
+logging.info(f"INDEX_ID is {INDEX_ID}")
+
+EMBED_MODEL_ID = "sentence-transformers/all-MiniLM-L6-v2"
+logging.info(f"EMBED_MODEL_ID is {EMBED_MODEL_ID}")
+
+PINECONE_ENVIRONMENT_ID = "gcp-starter"
+logging.info(f"PINECONE_ENVIRONMENT_ID is {PINECONE_ENVIRONMENT_ID}")
+
+
+# Pyppeteer configuration
+FROM_DOCKER = os.getenv("FROM_DOCKER")
+if not FROM_DOCKER:
+    logging.info("Running web scraping locally")
+    FROM_DOCKER = False
+else:
+    logging.info("Running web scraping from inside Docker")
+    FROM_DOCKER = True
+
+# Bot inference settings
+INFERENCE_SETTINGS = {
+    'use_rag': False,
+}
+
+# Model initialization settings
+MODEL_INIT_SETTINGS = {
+    'model_id': MODEL_ID,
+    'index_id': INDEX_ID,
+    'embed_model_id': EMBED_MODEL_ID,
+    'pinecone_environment_id': PINECONE_ENVIRONMENT_ID,
+}
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ai_articles_assistant_bot",
+    version="1.0",
+    description="A package answering questions from papers via Telegram bot",
+    url="https://github.com/TheGravityZero/AI-articles-assistant-bot",
+    author="Ilya Moiseenkov, Ruslan Gaptulkhakov",
+    packages=find_packages(),
+)
